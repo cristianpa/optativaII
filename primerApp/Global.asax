d0bd345b36a03e47ee1c7f5ef55b8b02d4b3cc3f
cristian <cristianfer_p@yahoo.es>
2014-07-02 18:01:29 -0500

    Initial commit

---
+++ primerApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="primerApp.MvcApplication" Language="C#" %>
---
+++ primerApp/Models/Alumno.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace primerApp.Models
+{
+    public class Alumno
+    {
+        
+
+        public int AlumnoId { get; set; }
+                    
+        public string Nombre { get; set; }
+
+    }
+}
